566f4331de41efeb455ccfdbd803c96669c37067
cademccu <cademccu@rams.colostate.edu>
2020-05-12 17:20:34 -0600

    set up package and test class

---
+++ getopt_java/getopt.java
@@ -0,0 +1,14 @@
+package getopt_java;
+
+public class getopt {
+
+	protected getopt() {
+		System.out.println("constructed");
+	}
+
+	protected void test() {
+		System.out.println("method");
+	}
+
+
+} // end getopt class
